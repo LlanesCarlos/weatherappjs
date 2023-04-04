ee8ab1828de5c31b40ff8a71b8d660d79414c46b
“LlanesCarlos” <cllanes.llanes@gmail.com>
2023-04-04 01:23:16 -0400

    Ejemplo de mini proyecto.

---
+++ src/components/weatherMainInfo.js
@@ -0,0 +1,15 @@
+export default function WeatherMainInfo({weather}){
+    return <div>
+        <div>{weather?.location.name}</div>
+        <div>{weather?.location.country}</div>
+        <div>
+            <div>
+                <img src={`http:${weather?.current.condition.icon}`} width="128" alt={weather?.current.condition.text} />
+            </div>
+        </div>
+        <div>
+            <div>{weather?.current.condition.text}</div>
+            <div>{weather?.current.temp_c}</div>
+        </div>
+    </div>
+}
